ddb8790ce6c5ca736b3f2ad41da63d9701768d82
haasj22 <49049551+haasj22@users.noreply.github.com>
2020-10-26 22:06:50 -0700

    Add files via upload

--- video_conversion.py
+++ video_conversion.py
@@ -6,14 +6,14 @@ if not os.path.exists('gopro_vids'):
 for filename in os.listdir('Recycle'):
     if filename.startswith('.'):
         continue
-    if not os.path.exists('gopro_vids/' + str(filename)):
-        os.makedirs('gopro_vids' + str(filename))
-    vidcap = cv2.VideoCapture('big_buck_bunny_720p_5mb.mp4')
+    if not os.path.exists('gopro_vids/' + str(filename)[:-4]):
+        os.makedirs('gopro_vids/' + str(filename)[:-4])
+    vidcap = cv2.VideoCapture("Recycle/" + str(filename))
     success,image = vidcap.read()
     count = 0
     while success:
-        if (count%5 == 0):
-            cv2.imwrite("gopro_vids/" + str(filename) + "/frame%d.jpg" % count, image)     # save frame as JPEG file      
+        print(count)
+        cv2.imwrite("gopro_vids/" + str(filename)[:-4] + "/frame%d.jpg" % count, image)     # save frame as JPEG file      
         success,image = vidcap.read()
         print('Read a new frame: ', success)
         count += 1
